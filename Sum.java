c938e8c37785dc1e5887b78152cf17f5d83cfcba
mazumder-anik <144948053+mazumder-anik@users.noreply.github.com>
2024-07-27 21:18:04 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String operator = sc.next();
+
+        if (Objects.equals(operator, "+")) {
+            System.out.println(a+b);
+        } else if (Objects.equals(operator, "-")) {
+            System.out.println(a-b);
+        } else if (Objects.equals(operator, "*")) {
+            System.out.println(a * b);
+        } else if (Objects.equals(operator, "/")) {
+            System.out.println(a/b);
+        } else if (Objects.equals(operator, "%")) {
+            System.out.println(a%b);
+        }else {
+            System.out.println("invalid operator");
+        }
+
+    }
+}
