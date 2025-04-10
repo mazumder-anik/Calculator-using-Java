82661185c538344e2b1e5bd076fbf85875af279d
Anik Mazumder <144948053+mazumder-anik@users.noreply.github.com>
2025-04-11 00:34:39 +0530

    Update Calculator.java
    It was done in only using if-else statement. Now it'supdated with switch case.

--- Calculator.java
+++ Calculator.java
@@ -1,24 +1,60 @@
+// version 1.0
+
+// import java.util.*;
+
+// public class Calculator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         String operator = sc.next();
+
+//         if (Objects.equals(operator, "+")) {
+//             System.out.println(a+b);
+//         } else if (Objects.equals(operator, "-")) {
+//             System.out.println(a-b);
+//         } else if (Objects.equals(operator, "*")) {
+//             System.out.println(a * b);
+//         } else if (Objects.equals(operator, "/")) {
+//             System.out.println(a/b);
+//         } else if (Objects.equals(operator, "%")) {
+//             System.out.println(a%b);
+//         }else {
+//             System.out.println("invalid operator");
+//         }
+
+//     }
+// }
+
+// Version 2.0
+
 import java.util.*;
 
-public class Calculator {
+public class Javabasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        String operator = sc.next();
+        char operator = sc.next().charAt(0);
 
-        if (Objects.equals(operator, "+")) {
-            System.out.println(a+b);
-        } else if (Objects.equals(operator, "-")) {
-            System.out.println(a-b);
-        } else if (Objects.equals(operator, "*")) {
-            System.out.println(a * b);
-        } else if (Objects.equals(operator, "/")) {
-            System.out.println(a/b);
-        } else if (Objects.equals(operator, "%")) {
-            System.out.println(a%b);
-        }else {
-            System.out.println("invalid operator");
+        switch (operator) {
+            case '+':
+                System.out.println("The addition is:" + (a + b));
+                break;
+            case '-':
+                System.out.println("The subtraction is:" + (a - b));
+                break;
+            case '*':
+                System.out.println("The multiplication is:" + (a * b));
+                break;
+            case '/':
+                System.out.println("The divition is:" + (a / b));
+                break;
+            case '%':
+                System.out.println("The modulo is:" + (a % b));
+                break;
+            default:
+                System.out.println("Wrong Operator");
         }
 
     }
